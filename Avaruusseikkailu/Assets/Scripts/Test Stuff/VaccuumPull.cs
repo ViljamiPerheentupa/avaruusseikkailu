72f7551e19c6389f0bd2238315184275112cc39b
Viljami Perheentupa <pviljo@gmail.com>
2020-01-19 02:54:30 +0200

    GameScene aloitettu

---
+++ Avaruusseikkailu/Assets/Scripts/Test Stuff/VaccuumPull.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VaccuumPull : MonoBehaviour
+{
+    public Transform pullPoint;
+    public float speed;
+    public ForceMode forceMode;
+    public LayerMask mask;
+    BoxCollider col;
+    Vector3 size;
+    public bool testing;
+    void Start()
+    {
+        col = GetComponent<BoxCollider>();
+        size = col.bounds.size;
+    }
+
+    void Update()
+    {
+        if (testing) {
+            if (Input.GetKeyDown(KeyCode.F5)) {
+                Pull();
+            }
+        }
+    }
+
+    public void Pull() {
+        Collider[] objects = Physics.OverlapBox(transform.position, size / 2, transform.rotation, mask);
+        foreach (Collider obj in objects) {
+            obj.GetComponent<Rigidbody>().AddForce((pullPoint.position - obj.transform.position) * speed, forceMode);
+        }
+    }
+}
